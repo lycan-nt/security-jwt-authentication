10bb5f32b2a25655ff4bf7185502af1c71d7ae6f
Felipe D. Santos <lycan.felipe@gmail.com>
2022-09-24 09:42:21 -0300

    Up project

---
+++ src/main/java/com/owl/securityjwt/TestController.java
@@ -0,0 +1,31 @@
+package com.owl.securityjwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Home Page";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('USER')")
+    public String profile(Authentication authentication) {
+        log.info("Username={}, roles={}", authentication.getPrincipal(), authentication.getAuthorities());
+        return "profile page";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String admin() {
+        return "Admin Page";
+    }
+
+}
